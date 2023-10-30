d300323d2034b5bb3ec87cf9c3cb898f062e0d8a
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-10-30 12:58:15 +0530

    main.py updated for data ingestion&tested

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from mlproject import logger
+from mlproject.pipeline import DataIngestionTrainingPipeline
 
-logger.info("logging started")
+
+
+STAGE_NAME = "Data Ingestion"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
