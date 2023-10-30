36dae6fec5aeb5ac955be9f40df494aeadb4c710
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-10-30 18:59:19 +0530

    data validation updated in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mlproject import logger
 from mlproject.pipeline import DataIngestionTrainingPipeline
+from mlproject.pipeline import DataValidationTrainingPipeline
 
 
 
@@ -7,6 +8,17 @@ STAGE_NAME = "Data Ingestion"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Data Validation"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
