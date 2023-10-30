30f677fede276c276ee024b84d4b6dd0564ce4d4
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-10-30 21:19:36 +0530

    model trainer updated & tested

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from mlproject import logger
 from mlproject.pipeline import DataIngestionTrainingPipeline
 from mlproject.pipeline import DataValidationTrainingPipeline
 from mlproject.pipeline import DataTransformationTrainingPipeline
+from mlproject.pipeline import ModelTrainerTrainingPipeline
 
 
 
@@ -31,6 +32,17 @@ STAGE_NAME = "Data Transformation"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
    data_ingestion.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
