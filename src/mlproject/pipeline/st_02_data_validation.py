ffb128b349d66aeca810878b25af4c7882516d72
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-10-30 18:58:11 +0530

    pipeline added for datavalidation

---
+++ src/mlproject/pipeline/st_02_data_validation.py
@@ -0,0 +1,27 @@
+from mlproject.config import ConfigurationManager
+from mlproject.components import DataValiadtion
+from mlproject import logger
+
+
+STAGE_NAME = "Data Validation"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_columns()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
