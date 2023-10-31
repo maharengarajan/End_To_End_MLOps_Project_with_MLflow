4eebf2123263c764ca8668d433e91cbc02681a42
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-10-31 16:33:37 +0530

    configuration added for model evaluation

--- src/mlproject/config/configuration.py
+++ src/mlproject/config/configuration.py
@@ -1,6 +1,12 @@
 from mlproject.constants import *
 from mlproject.utils.common import read_yaml, create_directories
-from mlproject.entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
+from mlproject.entity import (
+    DataIngestionConfig,
+    DataValidationConfig,
+    DataTransformationConfig,
+    ModelTrainerConfig,
+    ModelEvaluationConfig,
+)
 
 
 class ConfigurationManager:
@@ -8,11 +14,11 @@ class ConfigurationManager:
         self,
         config_file_path=CONFIG_FILE_PATH,
         params_file_path=PARAMS_FILE_PATH,
-        schema_file_path=SCHEMA_FILE_PATH):
-
-        self.config=read_yaml(config_file_path)
-        self.params=read_yaml(params_file_path)
-        self.schema=read_yaml(schema_file_path)
+        schema_file_path=SCHEMA_FILE_PATH,
+    ):
+        self.config = read_yaml(config_file_path)
+        self.params = read_yaml(params_file_path)
+        self.schema = read_yaml(schema_file_path)
 
         create_directories([self.config.artifacts_root])
 
@@ -20,16 +26,16 @@ class ConfigurationManager:
         config = self.config.data_ingestion
 
         create_directories([config.root_dir])
-        
+
         data_ingestion_config = DataIngestionConfig(
             root_dir=config.root_dir,
             source_URL=config.source_URL,
             local_data_file=config.local_data_file,
-            unzip_dir=config.unzip_dir
+            unzip_dir=config.unzip_dir,
         )
 
         return data_ingestion_config
-    
+
     def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
         schema = self.schema.COLUMNS
@@ -39,12 +45,12 @@ class ConfigurationManager:
         data_validation_config = DataValidationConfig(
             root_dir=config.root_dir,
             status_file=config.status_file,
-            unzip_data_dir = config.unzip_data_dir,
+            unzip_data_dir=config.unzip_data_dir,
             all_schema=schema,
         )
 
         return data_validation_config
-    
+
     def get_data_transformation_config(self) -> DataTransformationConfig:
         config = self.config.data_transformation
 
@@ -56,23 +62,41 @@ class ConfigurationManager:
         )
 
         return data_transformation_config
-    
+
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         config = self.config.model_trainer
         params = self.params.ElasticNet
-        schema =  self.schema.TARGET_COLUMN
+        schema = self.schema.TARGET_COLUMN
 
         create_directories([config.root_dir])
 
         model_trainer_config = ModelTrainerConfig(
             root_dir=config.root_dir,
-            train_data_path = config.train_data_path,
-            test_data_path = config.test_data_path,
-            model_name = config.model_name,
-            alpha = params.alpha,
-            l1_ratio = params.l1_ratio,
-            target_column = schema.name
-            
+            train_data_path=config.train_data_path,
+            test_data_path=config.test_data_path,
+            model_name=config.model_name,
+            alpha=params.alpha,
+            l1_ratio=params.l1_ratio,
+            target_column=schema.name,
         )
 
         return model_trainer_config
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+        config = self.config.model_evaluation
+        params = self.params.ElasticNet
+        schema = self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_evaluation_config = ModelEvaluationConfig(
+            root_dir=config.root_dir,
+            test_data_path=config.test_data_path,
+            model_path=config.model_path,
+            all_params=params,
+            metric_file_name=config.metric_file_name,
+            target_column=schema.name,
+            mlflow_uri="https://dagshub.com/maharengarajan/End_To_End_MLOps_Project_with_MLflow.mlflow",
+        )
+
+        return model_evaluation_config
